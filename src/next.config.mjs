003d3902073bf01381ddd3efaa8896cfc485a6eb
Angelica Guemes <97576442+AngelicaGE@users.noreply.github.com>
2025-02-12 18:32:28 -0600

    add upload image feature with s3 (#6)

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["m.media-amazon.com", "mdbproductsbucket.s3.us-east-2.amazonaws.com"]
+        domains: ["m.media-amazon.com", "mdbproductsbucket.s3.us-east-2.amazonaws.com", "product-description-generator.s3.us-east-1.amazonaws.com"]
     }
 };
 
