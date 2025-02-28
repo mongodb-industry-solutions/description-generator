9b1a9a95871c158150ebfcd3999d06f83d3be5e1
Angelica Guemes <97576442+AngelicaGE@users.noreply.github.com>
2025-02-28 11:38:20 -0600

    Feature/product url (#12)
    * del(component): unused comp
    
    * promp(togetehrai): improve prompt to prevent hallucinations
    
    * front(url): add direct url to prods
    
    * front(redirect): generate desc from catalog page

--- src/app/catalog/page.js
+++ src/app/catalog/page.js
@@ -1,7 +1,8 @@
 "use client"
 
-import { deleteDescriptions, deleteProductFromMDB, fetchProducts } from '@/lib/api';
-import React, { useState, useEffect } from 'react';
+import { deleteDescriptions, fetchProducts } from '@/lib/api';
+import { useRouter } from "next/navigation";
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
     HeaderCell,
@@ -11,36 +12,28 @@ import {
     TableHead,
 } from '@leafygreen-ui/table';
 import Spinner from 'react-bootstrap/Spinner';
-
 //import Code from '@leafygreen-ui/code';
-
-import Image from 'next/image';
 import { Container } from 'react-bootstrap';
-import Button from "@leafygreen-ui/button";
 
 import './catalog.css'
-import { deleteProduct, deleteProductDescriptions, setInitialLoad, setOpenedProductDetails, setProducts } from '@/redux/slices/ProductsSlice';
-import ModalContainer from '@/components/modalContainer/ModalContainer';
-import JsonDisplay from '@/components/jsonDisplayComp/JsonDisplayComp';
+import { deleteProductDescriptions, setInitialLoad, setOpenedProductDetails, setProducts } from '@/redux/slices/ProductsSlice';
 import DescriptionInput from '@/components/descriptionInput/DescriptionInput';
 import { addOperationAlert, addSucAutoCloseAlertHnd, addWarnAutoCloseAlertHnd, closeAlertWithDelay } from '@/lib/alerts';
 import { setLengthFilter, setModelFilter } from '@/redux/slices/FormSlice';
 import ProductRow from '@/components/productRow/ProductRow';
 
 export default function Page() {
+    const router = useRouter();
     const dispatch = useDispatch();
-    const [open, setOpen] = useState(false)
     const catalog = useSelector(state => state.Products.products)
     const modelOptions = useSelector(state => state.Form.models)
     const lengthOptions = useSelector(state => state.Form.lengths)
     const openedProductDetails = useSelector(state => state.Products.openedProductDetails)
     const initialLoad = useSelector(state => state.Products.initialLoad)
-    const [disableActionsInModal, setDisableActionsInModal] = useState(false)
 
     const onDeleteDescriptions = (product) => {
         const delProductDescriptions = async () => {
             const deletingId = new Date()
-            setDisableActionsInModal(true)
             addOperationAlert({
                 id: deletingId.getMilliseconds(),
                 title: 'Delete operation',
@@ -69,54 +62,18 @@ export default function Page() {
                 })
                 console.error("There was a problem deleting the prodict's descriptions:", error);
             }
-            setDisableActionsInModal(false)
         };
         delProductDescriptions();
     }
     const onSeeFullDocument = (product) => {
-        setOpen(true)
         dispatch(setOpenedProductDetails(product))
+        router.push(`/catalog/${product._id}`);
     }
     const onModelChange = (selectedOption) => {
         dispatch(setModelFilter(selectedOption.value))
     }
     const onLengthChange = (selectedOption) => {
         dispatch(setLengthFilter(selectedOption.value))
-    }
-    const deleteOpenedProductDetails = async () => {
-        if (!openedProductDetails)
-            return
-        setDisableActionsInModal(true)
-        const deletingId = new Date()
-        addOperationAlert({
-            id: deletingId.getMilliseconds(),
-            title: 'Delete operation',
-            message: `Deleting product ${openedProductDetails._id}`
-        })
-        try {
-            const response = await deleteProductFromMDB({ _id: openedProductDetails._id, imageUrl: openedProductDetails.imageUrl });
-            setOpen(false)
-            dispatch(setOpenedProductDetails(null))
-            if (response.modifiedCount > 0 || response.acknowledged == true) {
-                dispatch(deleteProduct({ _id: response._id, imageUrl: response.imageUrl }))
-                addSucAutoCloseAlertHnd({
-                    id: (new Date()).getMilliseconds(),
-                    title: 'Delete operation',
-                    message: `Product ${openedProductDetails._id} deleted`,
-                    duration: 4000
-                })
-                closeAlertWithDelay(deletingId.getMilliseconds(), 1500)
-            }
-        } catch (error) {
-            addWarnAutoCloseAlertHnd({
-                id: (new Date()).getMilliseconds(),
-                title: 'Delete operation',
-                message: `Error deleting product`,
-                duration: 4000
-            })
-            console.error("There was a problem deleting the product:", error);
-        }
-        setDisableActionsInModal(false)
     }
 
     useEffect(() => {
@@ -197,53 +154,6 @@ export default function Page() {
                         </Table>
                 }
             </Container>
-            <ModalContainer
-                allowClose={true}
-                open={open}
-                setOpen={setOpen}
-                children={(open && openedProductDetails !== null)
-                    ? <div className=' d-flex flex-column align-items-center justify-content-center'>
-                        <h3>Product's document model</h3>
-                        {
-                            openedProductDetails &&
-                            <Image
-                                src={openedProductDetails?.imageUrl}
-                                width={120}
-                                height={120}
-                                style={{ objectFit: "contain", padding: '4px' }}
-                                alt='Product'
-                            ></Image>
-                        }
-                        <div>
-                            {
-                                !openedProductDetails?.imageUrl.includes('https://m.media-amazon.com/images') &&
-                                <Button
-                                    variant='default'
-                                    size='small'
-                                    disabled={disableActionsInModal}
-                                    className='mt-1'
-                                    onClick={() => deleteOpenedProductDetails()}
-                                >
-                                    Delete product
-                                </Button>
-                            }
-                            <Button
-                                variant='default'
-                                size='small'
-                                disabled={Object.keys(openedProductDetails.descriptions).length == 0 || disableActionsInModal}
-                                className='mt-1 ms-3'
-                                onClick={() => onDeleteDescriptions(openedProductDetails)}
-                            >
-                                Clear all descriptions
-                            </Button>
-                        </div>
-                        <JsonDisplay data={openedProductDetails} />
-                        {/* <Code language="json">{openedProductDetails}</Code> */}
-                    </div>
-                    : <Spinner animation="border" variant="secondary" />
-                }
-                onCloseCallback={() => dispatch(setOpenedProductDetails(null))}
-            />
         </div>
     );
 }
--- src/components/productRow/ProductRow.js
+++ src/components/productRow/ProductRow.js
@@ -1,25 +1,50 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux'
+import { useRouter } from "next/navigation";;
 import Image from 'next/image';
 
 import { Cell, Row } from '@leafygreen-ui/table';
 import CurlyBracesIcon from '@leafygreen-ui/icon/dist/CurlyBraces';
 import TrashIcon from '@leafygreen-ui/icon/dist/Trash';
 import IconButton from '@leafygreen-ui/icon-button';
+import CopyIcon from '@leafygreen-ui/icon/dist/Copy';
+import SparkleIcon from '@leafygreen-ui/icon/dist/Sparkle';
 import DescriptionOutput from '../descriptionOutput/DescriptionOutput';
+import { setProductImageInFormFromObjectId } from '@/lib/helpers';
 
 const ProductRow = (props) => {
+    const router = useRouter();
     let { product, onSeeFullDocument, onDeleteDescriptions } = props
 
     const selectedModel = useSelector(state => state.Form.models?.find(model => model.isSelectedFilter === true).value)
     const selectedLength = useSelector(state => state.Form.lengths?.find(length => length.isSelectedFilter === true).value)
 
+    const onCopyIdClick = () => {
+        navigator.clipboard.writeText(product._id);
+    }
+    const onGenerateDescription = () => {
+        router.push(`/`);
+        const imageId = setProductImageInFormFromObjectId(product._id)
+        console.log(imageId)
+    }
+
     return (
         <Row>
-            <Cell>{product._id}</Cell>
+            <Cell>
+                {product._id}
+                <IconButton
+                    aria-label="Some Menu"
+                    title='Copy'
+                    className='ms-2'
+                    onClick={() => onCopyIdClick()}
+                >
+                    <CopyIcon />
+                </IconButton>
+            </Cell>
             <Cell>
                 <div className='cursorPointer'>
                     <Image
+                        onClick={() => onSeeFullDocument(product)}
                         src={product?.imageUrl}
                         width={100}
                         height={100}
@@ -63,17 +88,24 @@ const ProductRow = (props) => {
                     !product.descriptions?.fr?.[`${selectedLength}_${selectedModel.replaceAll('.', '')}`]
                         ? 'N/A'
                         : <DescriptionOutput
-                        language='fr'
-                        languageLabel={null}
-                        description={product.descriptions?.fr?.[`${selectedLength}_${selectedModel.replaceAll('.', '')}`] || 'N/A'}
-                        imageUrl={product.imageUrl}
-                        length={selectedLength}
-                        model={selectedModel}
-                        isInCatalogRow={true}
-                    ></DescriptionOutput>
+                            language='fr'
+                            languageLabel={null}
+                            description={product.descriptions?.fr?.[`${selectedLength}_${selectedModel.replaceAll('.', '')}`] || 'N/A'}
+                            imageUrl={product.imageUrl}
+                            length={selectedLength}
+                            model={selectedModel}
+                            isInCatalogRow={true}
+                        ></DescriptionOutput>
                 }
             </Cell>
             <Cell>
+                <IconButton
+                    onClick={() => onGenerateDescription(product)}
+                    aria-label="Some Menu"
+                    title='Generate description'
+                >
+                    <SparkleIcon />
+                </IconButton>
                 <IconButton
                     onClick={() => onSeeFullDocument(product)}
                     aria-label="Some Menu"
