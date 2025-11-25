6a133d0a84e852a7f7529bac3faa2783acd7b619
angie <angie.guemes@mongodb.com>
2025-11-24 19:26:43 -0600

    update connection to mongo logic

--- src/app/api/deleteProduct/route.js
+++ src/app/api/deleteProduct/route.js
@@ -1,14 +1,16 @@
 import { NextResponse } from "next/server";
-import { connectToDatabase, closeDatabase } from "@/lib/mongodb";
+import { clientPromise, dbName } from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
 
 export async function POST(request) {
-  const dbName = process.env.DB_NAME || "TogetherMDB";
   const collectionName = process.env.COLLECTION_NAME || "product";
 
   try {
     let { _id, imageUrl } = await request.json();
-    const collection = await connectToDatabase(dbName, collectionName);
+    const client = await clientPromise;
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    
     const filter = {
         '_id': ObjectId.createFromHexString(_id),
         'imageUrl': imageUrl
@@ -20,7 +22,5 @@ export async function POST(request) {
   } catch (error) {
     console.error("Error removing documents:", error);
     return NextResponse.json({ error: error.message }, { status: 500 });
-  } finally {
-    //await closeDatabase();
   }
 }
--- src/app/api/getCatalog/route.js
+++ src/app/api/getCatalog/route.js
@@ -1,19 +1,18 @@
 import { NextResponse } from "next/server";
-import { connectToDatabase, closeDatabase } from "@/lib/mongodb";
+import { clientPromise, dbName } from "@/lib/mongodb";
 
 export async function POST() {
-  const dbName = process.env.DB_NAME || "TogetherMDB";
   const collectionName = process.env.COLLECTION_NAME || "product";
 
   try {
-    const collection = await connectToDatabase(dbName, collectionName);
+    const client = await clientPromise;
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
     const documents = await collection.find({}).sort({ _id: -1 }).toArray();
 
     return NextResponse.json(documents);
   } catch (error) {
     console.error("Error fetching documents:", error);
     return NextResponse.json({ error: error.message }, { status: 500 });
-  } finally {
-    //await closeDatabase();
   }
 }
